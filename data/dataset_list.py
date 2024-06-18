c89d81dde0963c2e85a7dd28ceb18968683a5d3e
setsutt <77319813+setsutt@users.noreply.github.com>
2024-06-18 13:56:00 +0800

    Add files via upload

---
+++ data/dataset_list.py
@@ -0,0 +1,27 @@
+# Copyright 2024 Arjun Ashok
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# "kdd_cup_2018_without_missing","australian_electricity_demand", "electricity_hourly", "london_smart_meters_without_missing", "solar_10_minutes", "wind_farms_without_missing", "pedestrian_counts","saugeenday","sunspot_without_missing",
+# "traffic", "uber_tlc_hourly","exchange_rate", "cpu_limit_minute", "cpu_usage_minute", "function_delay_minute","requests_minute",
+#  "platform_delay_minute","instances_minute", "memory_limit_minute", "memory_usage_minute", "ett_h1", "ett_h2", "ett_m1", "ett_m2", "beijing_pm25", "AirQualityUCI", "beijing_multisite"
+# ALL_DATASETS = ["my", "traffic", "uber_tlc_hourly","exchange_rate", "cpu_limit_minute", "cpu_usage_minute", "function_delay_minute",
+#                 "requests_minute", "platform_delay_minute","instances_minute", "memory_limit_minute", "memory_usage_minute",
+#                 "ett_h1", "ett_h2", "ett_m1", "ett_m2", "beijing_pm25", "AirQualityUCI", "beijing_multisite"]"30a","31a","32a"]#
+# ALL_DATASETS = ["30_0","30_1", "31_0","31_1","32_0","32_1","33_0","33_1", "34_0","34_1","35_0","35_1","36_0","36_1", "37_0","37_1","38_0","38_1","39_0","39_1", "41_0","41_1",
+#               "43_0","43_1", "44_0","44_1","45_0","45_1","46_0","46_1", "47_0","47_1","48_0","48_1", "50_0","50_1","51_0","51_1", "52_0","52_1",]
+
+
+# ALL_DATASETS = ["14046_0","14046_1", "14134_0","14134_1","14149_0","14149_1","14157_0","14157_1", "14172_0","14172_1",
+#                   "14184_0","14184_1","15814_0","15814_1"]
+
+ALL_DATASETS = ["16265_0", "16272_0","16273_0", "16420_0", "16483_0", "16539_0", "16773_0", "16786_0", "16795_0", "17052_0", "17453_0", "18177_0", "18184_0", "19088_0", "19090_0","19093_0", "19140_0", "19830_0"]
