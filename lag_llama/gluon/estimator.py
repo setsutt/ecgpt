062e41032a896f1f2fd50e8b5643634620acfbc3
setsutt <77319813+setsutt@users.noreply.github.com>
2024-12-17 15:38:14 +0800

    Update estimator.py

--- lag_llama/gluon/estimator.py
+++ lag_llama/gluon/estimator.py
@@ -167,7 +167,7 @@ class LagLlamaEstimator(PyTorchLightningEstimator):
         # else:
         #     self.lags_seq = [] #如果lag_indices列表为空（即lags_seq没有生成任何滞后索引），则self.lags_seq被设置为空列表。
 
-        self.lags_seq = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,14,18,19,20]
+        self.lags_seq = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,14,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
 
         self.n_head = n_head
         self.n_layer = n_layer
